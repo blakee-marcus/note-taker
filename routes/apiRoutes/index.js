1e6fadedf0d0c58b4310afb3d4ddaa6fc2abb53f
bm <marcusb733@gmail.com>
2022-05-20 16:25:27 -0700

    Set Up GET Routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,21 @@
+const { validateNote, createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of hte array will be
+    req.body.id = notes.length.toString();
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
